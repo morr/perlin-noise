23cfa414bfcf01b8d3b76619913505a597707067
Andrey Sidorov <takandar@gmail.com>
2025-03-23 15:14:11 +0300

    updates to debug grid

--- src/debug_grid.rs
+++ src/debug_grid.rs
@@ -1,23 +1,17 @@
-use super::*;
 use super::grid::grid_tile_edge_to_world;
+use super::*;
 
 pub struct DebugGridPlugin;
 impl Plugin for DebugGridPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(
-            Update,
-            render_grid
-        );
+        app.add_systems(Update, render_grid);
     }
 }
 
 pub fn render_grid(mut gizmos: Gizmos) {
     for i in 0..GRID_SIZE {
         gizmos.line_2d(
-            Vec2::new(
-                grid_tile_edge_to_world(0),
-                grid_tile_edge_to_world(i),
-            ),
+            Vec2::new(grid_tile_edge_to_world(0), grid_tile_edge_to_world(i)),
             Vec2::new(
                 grid_tile_edge_to_world(GRID_SIZE),
                 grid_tile_edge_to_world(i),
@@ -28,10 +22,7 @@ pub fn render_grid(mut gizmos: Gizmos) {
 
     for i in 0..GRID_SIZE {
         gizmos.line_2d(
-            Vec2::new(
-                grid_tile_edge_to_world(i),
-                grid_tile_edge_to_world(0),
-            ),
+            Vec2::new(grid_tile_edge_to_world(i), grid_tile_edge_to_world(0)),
             Vec2::new(
                 grid_tile_edge_to_world(i),
                 grid_tile_edge_to_world(GRID_SIZE),
@@ -40,15 +31,22 @@ pub fn render_grid(mut gizmos: Gizmos) {
         );
     }
 
-    gizmos.arrow_2d(
-        Vec2::new(0.0, 0.0),
-        Vec2::new(GRID_SIZE as f32 * TILE_SIZE, 0.0),
+    gizmos.line_2d(
+        Vec2::new(grid_tile_edge_to_world(0), grid_tile_edge_to_world(0)),
+        Vec2::new(
+            grid_tile_edge_to_world(GRID_SIZE),
+            grid_tile_edge_to_world(0),
+        ),
         Color::srgb(1.0, 0.0, 0.0),
     );
 
-    gizmos.arrow_2d(
-        Vec2::new(0.0, 0.0),
-        Vec2::new(0.0, GRID_SIZE as f32 * TILE_SIZE),
+    gizmos.line_2d(
+        Vec2::new(grid_tile_edge_to_world(0), grid_tile_edge_to_world(0)),
+        Vec2::new(
+            grid_tile_edge_to_world(0),
+            grid_tile_edge_to_world(GRID_SIZE),
+        ),
         Color::srgb(0.0, 1.0, 0.0),
     );
 }
+
