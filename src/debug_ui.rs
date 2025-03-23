f2747d67c63d3f3e57ae19f44b8573af3daf5dd3
Andrey Sidorov <takandar@gmail.com>
2025-03-23 15:49:37 +0300

    debug ui

---
+++ src/debug_ui.rs
@@ -0,0 +1,36 @@
+use super::*;
+
+pub struct DebugUiPlugin;
+
+impl Plugin for DebugUiPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, render_debug_ui_info);
+    }
+}
+
+pub fn render_debug_ui_info(mut commands: Commands) {
+    commands.spawn((
+        Text::new(
+            // \"r\" - rebuild map
+            "\"space\" - generate perlin noise
+\"g\" - toggle grid",
+        ),
+        TextFont {
+            font_size: 12.,
+            ..default()
+        },
+        TextColor(Color::srgba(1.0, 1.0, 1.0, 0.75)),
+        Node {
+            position_type: PositionType::Absolute,
+            bottom: Val::Px(5.0),
+            left: Val::Px(5.0),
+            margin: UiRect {
+                top: Val::Px(8.0),
+                right: Val::Px(0.0),
+                bottom: Val::Px(0.0),
+                left: Val::Px(0.0),
+            },
+            ..default()
+        },
+    ));
+}
--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,23 @@ use crate::camera::CameraPlugin;
 mod grid;
 use crate::grid::GridPlugin;
 
+mod debug_ui;
+use crate::debug_ui::DebugUiPlugin;
+
 mod debug_grid;
 use crate::debug_grid::DebugGridPlugin;
 
 mod noise;
 use crate::noise::NoisePlugin;
 
-const GRID_SIZE: i32 = 250;
+const GRID_SIZE: i32 = 100;
 const TILE_SIZE: f32 = 10.0;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins((CameraPlugin, GridPlugin, DebugGridPlugin, NoisePlugin))
+        .add_plugins((CameraPlugin, GridPlugin, NoisePlugin))
+        .add_plugins((DebugGridPlugin, DebugUiPlugin))
         .add_plugins(FpsOverlayPlugin {
             config: FpsOverlayConfig {
                 text_config: TextFont {
@@ -35,7 +39,7 @@ fn main() {
                     font_smoothing: FontSmoothing::default(),
                 },
                 // We can also change color of the overlay
-                text_color: Color::srgba(1.0, 1.0, 1.0, 0.6),
+                text_color: Color::srgba(1.0, 1.0, 1.0, 0.75),
                 enabled: true,
             },
         })
