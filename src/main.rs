468264a1cb5afb4bb0a3a2b4625b50f126a7805d
Andrey Sidorov <takandar@gmail.com>
2025-03-23 13:31:26 +0300

    project skeleton

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        // .init_resource::<NoiseSettings>()
+        // .add_systems(Startup, setup)
+        // .add_systems(Update, (update_noise, keyboard_input_system))
+        .run();
+}
